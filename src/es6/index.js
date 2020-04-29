3e5507b4a4efe75ab36565126ce2a8da93feb39e
Camilo Hurtado <chc201627@gmail.com>
2020-04-28 20:25:15 -0500

    LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -5,8 +5,63 @@ function  newFunction (name, age, country){
     console.log(name,age,country)
 }
 
+//S6
 function newFunction2(name = 'Oscar',age = 32,country='MX'){
     console.log(name,age, country)
 }
 newFunction2();
 newFunction2('Camilo', 22, 'CO')
+
+//Template literal
+
+let hello = "Hello"
+let world = "World";
+
+let epicPhrase = hello +" " +world;
+console.log(epicPhrase);
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2)
+
+
+let lorem  = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsum suscipit fuga culpa deserunt dolores provident accusantium, fugit,\n" + "eius sequi, qui voluptatibus natus nisi at numquam. Praesentium, dolores ex. Quae! \n"
++ "Camilo"
+//ES6
+let lorem2  = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti nesciunt 
+quis deleniti saepe sed iste, et error odit culpa? Voluptates sit blanditiis aperiam asperiores nemo neque veniam similique accusamus dolores!`;
+console.log(lorem)
+console.log(lorem2)
+
+//Destructuracion elementos
+let person = {
+    'name': 'Camilo',
+    'age': 30,
+    'country':'COL'
+}
+console.log(person.name,person.age)
+//ES6
+let{name,age,country} = person;
+console.log(name,age,country)
+
+//Propagacion
+
+let team1 = ['Oscar','Camilo','Julian']
+let team2 = ['Martha','Camila','Carolina']
+
+let education = ['David',...team1,...team2]
+console.log(team1)
+console.log(team2)
+console.log(education)
+
+// Declaracion de variables
+
+{
+    var global = "global"
+
+}
+{
+    let globalLet = 'Global Let'
+}
+console.log(global)
+console.log(globalLet)
+
+const a  = 'b'
